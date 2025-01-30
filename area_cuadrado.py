7facfeaa1df14180ec821b85dca9a5e26e987118
nicolassarmiento28 <109758852+nicolassarmiento28@users.noreply.github.com>
2025-01-29 22:43:43 -0300

    Create area_cuadrado.py
    Proyecto de un area de un cuadrado

---
+++ area_cuadrado.py
@@ -0,0 +1,11 @@
+# CALCULAR AREA DE UN CUADRADO
+
+# 1 - SOLICITAR LA LONGITUD DEL LADO
+longitud_lado = int(input("Introduzca la longitud de un Lado del cuadrado en cm :"))
+print (f"la longitud del ladc proporcionado es : {longitud_lado}")
+
+# 2 - CALCULAR EL AREA
+area_cuadrado = longitud_lado * longitud_lado
+
+# 3- MOSTRAR EL RESULTADO
+print(f"el resultado del area del cuadrado en cm es : {area_cuadrado}")
